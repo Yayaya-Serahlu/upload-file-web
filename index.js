6aa6c3264d7b28a1d9d483c63528c253c70aadef
Yayaya-Serahlu <rafiqmarwan200@gmail.com>
2025-03-05 13:43:45 +0700

    y

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const port = 3000;
 const a = 'g'
 const b = 'h'
 const c = 'p'
-const to = '_bxqsHOyZgmjEDH'
-const ken = 'bQCGmhgGLQakGpFM09ap42'
+const to = '_KjTxuvo2kJHN86x'
+const ken = 'x8rSxyLeFvMSnHJ0G6RNz'
 const githubToken = `${a}${b}${c}${to}${ken}`; // https://github.com/settings/tokens
-const owner = 'Yayaya-Serahlu'; // GitHub username
-const repo = 'uploadfile'; // Repository name
+const owner = 'upload-file-lab'; // GitHub username
+const repo = 'fileupload'; // Repository name
 const branch = 'main';
 
 app.use(fileUpload());
@@ -30,7 +30,7 @@ app.post('/upload', async (req, res) => {
   let uploadedFile = req.files.file;
   let mimeType = mime.lookup(uploadedFile.name);
   let fileName = `${Date.now()}.${mime.extension(mimeType)}`;
-  let filePath = `readfile/${fileName}`;
+  let filePath = `uploads/${fileName}`;
   let base64Content = Buffer.from(uploadedFile.data).toString('base64');
 
   try {
@@ -45,14 +45,15 @@ app.post('/upload', async (req, res) => {
       },
     });
 
-    let rawUrl = `https://www.aplotpelrapikz.my.id/${filePath}`;
-   res.send(`
-   <!DOCTYPE html>
+    let rawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${filePath}`;
+    // YAYAYAYYAYAYAYAYAYAYYAYAYYAYYAYAYAYAA
+    res.send(`
+    <!DOCTYPE html>
 <html lang="id">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Upload Succes</title>
+  <title>Unggahan Berhasil</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
@@ -62,16 +63,30 @@ app.post('/upload', async (req, res) => {
         <img src="https://media.tenor.com/yWaLIc5J9WgAAAAj/momoi.gif" alt="Momoi GIF" class="mx-auto rounded-full h-32 w-32 object-cover">
       </div>
       <div class="text-center text-gray-700 mb-6">
-        File succes upload, URL result:
+        File succes upload, rawURL result:
       </div>
       <div class="text-center">
         <a id="rawUrlLink" href="${rawUrl}" class="text-blue-500 hover:text-blue-700">${rawUrl}</a>
       </div>
+      <div class="text-center mt-4">
+        <button onclick="copyUrl()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Salin URL</button>
+      </div>
     </div>
   </div>
+
+  <script>
+    function copyUrl() {
+      const rawUrl = document.getElementById('rawUrlLink').href;
+      navigator.clipboard.writeText(rawUrl).then(function() {
+        alert("URL berhasil disalin: " + rawUrl);
+      }).catch(function(error) {
+        alert("Gagal menyalin URL: " + error);
+      });
+    }
+  </script>
 </body>
 </html>
-   `);
+`);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error uploading file.');
