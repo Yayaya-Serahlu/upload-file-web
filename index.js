ab4a8ce98fd6bcf161d7a5d092fb422c82f3a9d2
Yayaya-Serahlu <rafiqmarwan200@gmail.com>
2025-08-16 07:15:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/uploadfile', async (req, res) => {
                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
                 </svg>
-                🔙 Back to Home
+                🔙 Back
             </a>
         </div>
     </div>
